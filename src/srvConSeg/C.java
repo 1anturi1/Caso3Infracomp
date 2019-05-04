a740cb71c2d0830550436d97be059ad1b5af5bd4
ANDRES IVAN ANTURI FIGUEROA <ai.anturi@sis.virtual.uniandes.edu.co>
2019-05-03 19:28:27 -0500

    poll de threads

--- src/srvConSeg/C.java
+++ src/srvConSeg/C.java
@@ -3,24 +3,31 @@ package srvConSeg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.KeyPair;
 import java.security.Security;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors ;
+import javax.management.Attribute;
+import javax.management.AttributeList;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 public class C {
 	private static ServerSocket ss;	
 	private static final String MAESTRO = "MAESTRO: ";
 	private static X509Certificate certSer; /* acceso default */
 	private static KeyPair keyPairServidor; /* acceso default */
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		ExecutorService ex = Executors.newFixedThreadPool(1) ;		
 		System.out.println(MAESTRO + "Establezca puerto de conexion:");
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -28,27 +35,47 @@ public class C {
 		System.out.println(MAESTRO + "Empezando servidor maestro en puerto " + ip);
 		// Adiciona la libreria como un proveedor de seguridad.
 		// Necesario para crear llaves.
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());		
-		
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());	
+
 		int idThread = 0;
 		// Crea el socket que escucha en el puerto seleccionado.
 		ss = new ServerSocket(ip);
 		System.out.println(MAESTRO + "Socket creado.");
-		
+
 		keyPairServidor = S.grsa();
 		certSer = S.gc(keyPairServidor);
 		D.initCertificate(certSer, keyPairServidor);
 		while (true) {
 			try { 
 				Socket sc = ss.accept();
-				System.out.println(MAESTRO + "Cliente " + idThread + " aceptado.");
-				D d = new D(sc,idThread);
+				System.out.println(MAESTRO + "Cliente " + idThread + " aceptado.");		
+				long startTime = System.nanoTime();
+				ex.submit( new D(sc,idThread)) ;	
+				long endTime = System.nanoTime() - startTime; 
+				
+				
+				
 				idThread++;
-				d.start();
+				
 			} catch (IOException e) {
 				System.out.println(MAESTRO + "Error creando el socket cliente.");
 				e.printStackTrace();
 			}
 		}
 	}
+
+	public double getSystemCpuLoad() throws Exception {
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		ObjectName name = ObjectName.getInstance("java.lang:type=OperatingSystem");
+		AttributeList list = mbs.getAttributes(name, new String[]{ "SystemCpuLoad" });
+		if (list.isEmpty()) return Double.NaN;
+		Attribute att = (Attribute)list.get(0);
+		Double value = (Double)att.getValue();
+		// usually takes a couple of seconds before we get real values
+		if (value == -1.0) return Double.NaN;
+		// returns a percentage value with 1 decimal point precision
+		return ((int)(value * 1000) / 10.0);
+	}
+
+
 }
--- src/srvSinSeg/C.java
+++ src/srvSinSeg/C.java
@@ -3,11 +3,20 @@ package srvSinSeg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.KeyPair;
 import java.security.Security;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors ;
+import javax.management.Attribute;
+import javax.management.AttributeList;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import srvConSeg.D;
 
 public class C {
 	private static ServerSocket ss;	
@@ -20,7 +29,8 @@ public class C {
 	 */
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		
+		ExecutorService ex = Executors.newFixedThreadPool(1) ;	
 		System.out.println(MAESTRO + "Establezca puerto de conexion:");
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -42,13 +52,28 @@ public class C {
 			try { 
 				Socket sc = ss.accept();
 				System.out.println(MAESTRO + "Cliente " + idThread + " aceptado.");
-				D d = new D(sc,idThread);
+				long startTime = System.nanoTime();
+				ex.submit( new D(sc,idThread)) ;	
+				long endTime = System.nanoTime() - startTime; 
 				idThread++;
-				d.start();
 			} catch (IOException e) {
 				System.out.println(MAESTRO + "Error creando el socket cliente.");
 				e.printStackTrace();
 			}
 		}
 	}
+	
+	public double getSystemCpuLoad() throws Exception {
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		ObjectName name = ObjectName.getInstance("java.lang:type=OperatingSystem");
+		AttributeList list = mbs.getAttributes(name, new String[]{ "SystemCpuLoad" });
+		if (list.isEmpty()) return Double.NaN;
+		Attribute att = (Attribute)list.get(0);
+		Double value = (Double)att.getValue();
+		// usually takes a couple of seconds before we get real values
+		if (value == -1.0) return Double.NaN;
+		// returns a percentage value with 1 decimal point precision
+		return ((int)(value * 1000) / 10.0);
+	}
+	
 }
